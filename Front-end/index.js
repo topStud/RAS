426d38c01879f9c0fdc541b90e46532bb6d23b2b
Topaz Tcherkass <topaz083@gmail.com>
2021-04-08 12:08:53 +0300

    we can now choose visually the search option (it will be marked). added a request for journal data on button click.

--- Front-end/index.js
+++ Front-end/index.js
@@ -1,21 +1,73 @@
-// the list needs to be sorted alphabetically
-let journal_names = [];
+// creates immutable object
+const searchOptions = Object.freeze({
+    JOURNAL_NAME: 'journalName',
+    SUBJECT_AREA: 'subjectArea',
+    ISSN: 'issn'
+})
 
-/**
- * getJournalNamesList function.
- * function that needs to be executed only ones to get the list.
- */
-$.ajax({
-    type: 'GET',
-    url: "../journals_names",
-    success: function (result) {
-        journal_names = result.sort()
-        $(document).ready(()=> {
-            // we start the functionality of autocomplete
-            autocomplete($('#searchInput'), journal_names)
-        });
-    },
-    error: function (XMLHttpRequest, textStatus) {
-        console.log(textStatus)
+let currentSearchOption = searchOptions.JOURNAL_NAME
+
+// getting the list of journal names for autocomplete
+sendGetRequest("../journals_names", function (result) {
+    $(document).ready(()=> {
+        // we start the functionality of autocomplete
+        autocomplete($('#searchInput'), result.sort())
+    });
+}, ajaxError)
+
+// on page load
+$(document).ready(() => {
+    $('#searchButton').on('click', onSearchClick)
+    $('#journalName').addClass('chosenSearchOption')
+    $('.search-option').on('click', onClickSearchOption)
+})
+
+function onClickSearchOption(event) {
+    $('#' + currentSearchOption).removeClass('chosenSearchOption')
+    currentSearchOption = event.target.id
+    $('#' + event.target.id).addClass('chosenSearchOption')
+    switch (currentSearchOption) {
+        case searchOptions.JOURNAL_NAME:
+            console.log(searchOptions.JOURNAL_NAME)
+            break
+        case searchOptions.SUBJECT_AREA:
+            console.log(searchOptions.SUBJECT_AREA)
+            break
+        case searchOptions.ISSN:
+            console.log(searchOptions.ISSN)
+            break
     }
-});
+}
+
+function onSearchClick() {
+    let input = $('#searchInput')
+    if (input.val() !== '') {
+        // clear input field
+        input.val('')
+        // request data of the journal
+        sendGetRequest('../journals_info', addDataToTable, ajaxError)
+    }
+}
+
+function addDataToTable(result) {
+    // let tbody = $('#journals-output')
+    // for (let i = 0; i < result.length; i++) {
+    //     let newRow = $('<tr></tr>')
+    //     // i need to add here columns
+    //     tbody.append(newRow)
+    // }
+    console.log('in success')
+}
+
+function sendGetRequest(url, sucFunc, errFunc) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: sucFunc,
+        error: errFunc
+    })
+}
+
+function ajaxError(XMLHttpRequest, textStatus) {
+    console.log(textStatus)
+}
