1a22c595f02a7d935874d1e8bdf8c187118c7b8d
Topaz Tcherkass <topaz083@gmail.com>
2021-05-26 15:35:57 +0300

    page navigation works

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,4 @@
 import './style/App.css';
-import AppMenu from './components/Navbar.js'
-import AppBody from './components/AppBody.js'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import PrivatePage from "./components/PrivatePage";
 import Home from "./components/Home";
