bf6940dca6c571183b27bcea969003a7ce8a6454
Topaz Tcherkass <topaz083@gmail.com>
2021-05-27 16:46:01 +0300

    sending request to server+checks --> login

--- front/src/components/LogIn.js
+++ front/src/components/LogIn.js
@@ -32,7 +32,54 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignIn() {
+    const [emailValue, setEmailValue] = React.useState('')
+    const [passValue, setPassValue] = React.useState('')
+    const [emailError, setEmailVError] = React.useState(false)
+    const [passError, setPassError] = React.useState(false)
+    const [emailMes, setEmailMes] = React.useState('')
+    const [passMes, passEmailMes] = React.useState('')
     const classes = useStyles();
+
+    function logInClicked() {
+        if (!ValidateEmail(emailValue) || passValue === '') {
+            if (!ValidateEmail(emailValue)) {
+                setEmailVError(true)
+                setEmailMes('wrong email format!')
+            }
+            if (passValue === '') {
+                setPassError(true)
+                passEmailMes('please enter your password!')
+            }
+        } else {
+            // removes the error annotation
+            setEmailVError(false)
+            setPassError(false)
+            setEmailMes('')
+            passEmailMes('')
+
+            let url = '/userInfo?email=' + emailValue + '&pass=' + passValue
+            // sends values to server for a check.
+            fetch(url).then(res => {
+                if (!res.ok) {
+                    throw Error(res.statusText);
+                }
+                return res.json()
+            }).then(data => {
+              // data = user id
+                window.open("/uid="+data);
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
+    }
+
+    function emailChange(e) {
+        setEmailValue(e.target.value)
+    }
+
+    function passChange(e) {
+        setPassValue(e.target.value)
+    }
 
     return (
         <div className={"App"}>
@@ -45,35 +92,43 @@ export default function SignIn() {
                     <Typography component="h1" variant="h5" color={'primary'}>
                         Log in
                     </Typography>
-                    <form className={classes.form} >
+                    <form className={classes.form} noValidate>
                         <TextField
                             variant="outlined"
                             margin="normal"
                             required
                             fullWidth
+                            error={emailError}
                             id="email"
                             label="Email Address"
                             name="email"
                             autoComplete="email"
                             autoFocus
+                            helperText={emailMes}
+                            value={emailValue}
+                            onChange={emailChange}
                         />
                         <TextField
                             variant="outlined"
                             margin="normal"
                             required
+                            error={passError}
                             fullWidth
                             name="password"
                             label="Password"
                             type="password"
                             id="password"
                             autoComplete="current-password"
+                            helperText={passMes}
+                            value={passValue}
+                            onChange={passChange}
                         />
                         <Button
-                            type="submit"
                             fullWidth
                             variant="contained"
                             color="primary"
                             className={classes.submit}
+                            onClick={() => logInClicked()}
                         >
                             Log In
                         </Button>
@@ -94,4 +149,9 @@ export default function SignIn() {
             </Container>
         </div>
     );
+}
+
+function ValidateEmail(email)
+{
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
 }
